97e22c9d965d60b01039f0d5bd406b54e9cf5938
Alucard-02 <55884435+Alucard-02@users.noreply.github.com>
2022-05-29 17:44:41 -0300

    Add files via upload

---
+++ tenis/src/tenis/Tenis.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tenis;
+
+/**
+ *
+ * @author nipi
+ */
+public class Tenis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        TenisJFrame Form = new TenisJFrame();
+        
+        Form.setVisible(true);
+    }
+    
+}
